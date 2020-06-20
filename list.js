2f9d1ae458b2a54a9b6ef0c1db58f6f4f8d28a92
nanditasharma <64246425+nanditasharma@users.noreply.github.com>
2020-06-20 15:18:26 +0545

    Add files via upload

---
+++ list.js
@@ -0,0 +1,71 @@
+var questions = [{
+	"question":" BCD stands for: ",
+    "option1":" Binary coded decimal ", 
+    "option2":" Binary coded decoded", 
+    "option3":"Both given option",
+    "option4":"none of these", 
+    "answer":"1"
+}, {
+	"question":" What is the octal equivalent of 58,3-bit binary number? ", 
+    "option1":" 0102 ", 
+    "option2":"1102", 
+    "option3":"0002",
+    "option4":"1012", 
+    "answer":"4" 
+}, {
+	"question":"The purpose of the microprocessor is to control_______ ", 
+    "option1":" memory ", 
+    "option2":"switches ", 
+    "option3":"processing  ",
+    "option4":"tasks ", 
+    "answer":"1" 
+}, {
+	"question":" The process of building new classes from existing one is called_______. ", 
+    "option1":"Polymorphism",
+    "option2": "Structure", 
+    "option3":"Inheritance",
+    "option4":"Cascading", 
+    "answer":"3" 
+}, {
+	"question":"Usually a pure virtual function ", 
+    "option1":"has complete function body.  ", 
+    "option2":"will never be called.", 
+    "option3":"will be called only to delete an object.",
+    "option4":"is defined only in derived class.",
+    "answer":"4" 
+}, {
+	"question":"This algorithm scans the list by swapping the entries whenever pair of adjacent keys are out of desired order.  ", 
+    "option1":"Insertion sort  ",
+    "option2": "Bubble sort", 
+    "option3":"Shell sort",
+    "option4":"Quick sort",
+    "answer":"2" 
+}, {
+	"question":"Viruses are ________ .  ", 
+    "option1":"Man made   ", 
+    "option2":"Naturally occur ", 
+    "option3":"Machine made ",
+    "option4": "All of the above", 
+    "answer":"1"
+}, {
+	"question":"In computers, subtraction is generally carried out by ", 
+    "option1":"9's complement",
+    "option2": "10's complement",
+    "option3": "1's complement",
+    "option4":"2's complement", 
+    "answer":"4" 
+}, {
+	"question":"The circuit used to store one bit of data is known as ", 
+    "option1":"Register", 
+    "option2":"Encoder",
+    "option3": "Decoder",
+    "option4":"Flip Flop",
+    "answer":"4"
+}, {
+	"question":"Floating point representation is used to store", 
+    "option1":"Boolean values", 
+    "option2":"whole numbers", 
+    "option3":"real integers",
+    "option4":"integers", 
+    "answer":"3"
+}];
